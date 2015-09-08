bc4778bbf437f07a7549a553930fa088441c3331
utsav93 <nick.utsav2345@gmail.com>
2015-09-08 16:57:56 -0600

    LevelRead.cs and LevelWrite.cs classes

---
+++ angry balls project 1.0/Angry Balls/Angry Balls/LevelRead.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Angry_Balls
+{
+    class LevelRead
+    {
+        public String title;
+        public void ReadXML()
+        {
+            // First write something so that there is something to read ... 
+            var b = new LevelRead { title = "Serialization Overview" };
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(LevelRead));
+            var wfile = new System.IO.StreamWriter(@"c:\temp\SerializationOverview.xml");
+            writer.Serialize(wfile, b);
+            wfile.Close();
+
+            // Now we can read the serialized level ...
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(LevelRead));
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                @"c:\temp\SerializationOverview.xml");
+            LevelRead overview = (LevelRead)reader.Deserialize(file);
+            file.Close();
+
+            Console.WriteLine(overview.title);
+
+        }
+    }
+}
---
+++ angry balls project 1.0/Angry Balls/Angry Balls/LevelWrite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml.XPath;
+using System.Xml;
+using System.Xml.Resolvers;
+using System.Xml.Schema;
+using System.Xml.Xsl;
+
+namespace Angry_Balls
+{
+    class LevelWrite
+    {
+        //WriteXML() to be called in save button class 
+
+
+        public class Book
+        {
+            public String title;
+        }
+
+
+        public static void WriteXML()
+        {
+            Book overview = new Book();
+            overview.title = "Serialization Overview";
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(Book));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, overview);
+            file.Close();
+        }
+    }
+}
+
